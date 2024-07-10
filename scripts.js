505e292c1c0b31e0fe604fa6c96644e81e3baf36
AFvisions <andrew@afvisions.com>
2024-07-10 00:32:57 -0500

    continued to make adjustments and improve responsivenes, made use of many media queries, its a bit jerky and not as fluid as I would like but will have to do.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
-// $(window).bind('beforeunload', function () {
-//     return 'Are you sure you want to leave?';
-// });
+$(window).bind('beforeunload', function () {
+    return 'Are you sure you want to leave?';
+});
