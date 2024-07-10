b226788e74b054738585389acb52274e4a6563e4
AFvisions <andrew@afvisions.com>
2024-07-10 01:36:29 -0500

    Two things. I noticed my Live Server using Firefox does not appear and function the same as my published page on Github. Could be an issue with Live Server. Also changed the original JS window to an alert that notifies the viewer of the previous statement.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
-$(window).bind('beforeunload', function () {
-    return 'Are you sure you want to leave?';
+jQuery(document).ready(function ($) {
+    alert("Missing functionality after publishing on Github Pages. e.g. No single column when minimized.");
 });
+
+
+// $(window).bind('beforeunload', function () {
+//     return 'Are you sure you want to leave?';
+// });
